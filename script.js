0c9a90b578ffbd47061b6d9d1150f168baa8a6ee
DEVLevid <levidlima80@gmail.com>
2023-05-14 13:43:14 -0300

    clean code

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 let coffee1 = document.querySelector('.thumb1');
 let coffee2 = document.querySelector('.thumb2');
 let coffee3 = document.querySelector('.thumb3');
+const circle = document.querySelector('.circle');
 
-coffee1.addEventListener("click",function(){
-        document.querySelector('.starbucks').src = "images/img1.png";
+coffee1.addEventListener("click", function () {
+    document.querySelector('.starbucks').src = "images/img1.png";
 });
-coffee2.addEventListener("click",function(){
+coffee2.addEventListener("click", function () {
     document.querySelector('.starbucks').src = "images/img2.png";
 });
-coffee3.addEventListener("click",function(){
+coffee3.addEventListener("click", function () {
     document.querySelector('.starbucks').src = "images/img3.png";
 });
 
-coffee1.addEventListener("click", function(){
-    const circle = document.querySelector('.circle');
+coffee1.addEventListener("click", function () {
     circle.style.background = "#017143";
 });
-coffee2.addEventListener("click",function(){
-    const circle = document.querySelector('.circle');
+coffee2.addEventListener("click", function () {
     circle.style.background = "#eb7495";
 });
-coffee3.addEventListener("click",function(){
-    const circle = document.querySelector('.circle');
+coffee3.addEventListener("click", function () {
     circle.style.background = "#d752b1";
 });
