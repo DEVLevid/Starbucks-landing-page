39f40b2e3840bc8da7ff69b4a0f37a5f1e253e96
DEVLevid <levidlima80@gmail.com>
2023-05-14 13:30:42 -0300

    external Javascript functions

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-function imgSlider(anything) {
-    document.querySelector('.starbucks').src = anything;
-};
+let coffee1 = document.querySelector('.thumb1');
+let coffee2 = document.querySelector('.thumb2');
+let coffee3 = document.querySelector('.thumb3');
 
-function changeCircleColor(color) {
+coffee1.addEventListener("click",function(){
+        document.querySelector('.starbucks').src = "images/img1.png";
+});
+coffee2.addEventListener("click",function(){
+    document.querySelector('.starbucks').src = "images/img2.png";
+});
+coffee3.addEventListener("click",function(){
+    document.querySelector('.starbucks').src = "images/img3.png";
+});
+
+coffee1.addEventListener("click", function(){
     const circle = document.querySelector('.circle');
-    circle.style.background = color;
-};
+    circle.style.background = "#017143";
+});
+coffee2.addEventListener("click",function(){
+    const circle = document.querySelector('.circle');
+    circle.style.background = "#eb7495";
+});
+coffee3.addEventListener("click",function(){
+    const circle = document.querySelector('.circle');
+    circle.style.background = "#d752b1";
+});
