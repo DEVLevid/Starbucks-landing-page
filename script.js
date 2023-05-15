3c0ef557634b959d1bcd1c1a25075e218e266eed
DEVLevid <levidlima80@gmail.com>
2023-05-15 17:20:14 -0300

    commit changes the js and css style

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let coffee1 = document.querySelector('.thumb1');
 let coffee2 = document.querySelector('.thumb2');
 let coffee3 = document.querySelector('.thumb3');
+
 const circle = document.querySelector('.circle');
+const menu = document.querySelector('.nav');
 
 coffee1.addEventListener("click", function () {
     document.querySelector('.starbucks').src = "images/img1.png";
@@ -22,5 +24,13 @@ coffee2.addEventListener("click", function () {
 coffee3.addEventListener("click", function () {
     circle.style.background = "#d752b1";
 });
+
+menu.addEventListener("click", function(){
+    if(!menu){
+        menu.style.background = "image/menu-regular-24.png"
+    } else{
+        menu.style.background = "image/x-regular-24.png"
+    }
+})
 
 
